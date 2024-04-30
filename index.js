cb69dbc1893db112a2e0234bfbc4ea8214e719b6
Niloy Das <niloydas5215@gmail.com>
2024-04-30 13:28:09 +0600

    updated cors

--- index.js
+++ index.js
@@ -3,13 +3,17 @@ const cors = require('cors')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const dotenv = require('dotenv')
 dotenv.config()
-const app = express()
 const port = process.env.PORT || 5000
+const app = express()
 
 
 // middleware
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173", "https://dream-destination-9ba69.firebaseapp.com"]
+}))
 app.use(express.json());
+
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.x3wylq5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -28,67 +32,67 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const touristsPlaceCollection = client.db("dreamDestinationDB").collection('touristsPlace');
     const countriesCollection = client.db("dreamDestinationDB").collection('countries');
 
-    app.get('/addTouristsSpot', async(req, res) => {
-        const cursor = touristsPlaceCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
+    app.get('/addTouristsSpot', async (req, res) => {
+      const cursor = touristsPlaceCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
     })
 
-    app.get('/countries', async(req, res) => {
-        const cursor = countriesCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
+    app.get('/countries', async (req, res) => {
+      const cursor = countriesCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
     })
 
-    app.get('/viewDetails/:id', async(req, res) => {
-        const id = req.params.id;
-        const query = {_id: new ObjectId(id)};
-        const result = await touristsPlaceCollection.findOne(query);
-        res.send(result);
+    app.get('/viewDetails/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await touristsPlaceCollection.findOne(query);
+      res.send(result);
     })
 
-    app.get('/viewDetailsForSpecificCountry/:country_Name', async(req, res) => {
-        const country_name = req.params.country_Name;
-        const cursor = touristsPlaceCollection.find({country_Name : country_name});
-        const result = await cursor.toArray();
-        res.send(result);
+    app.get('/viewDetailsForSpecificCountry/:country_Name', async (req, res) => {
+      const country_name = req.params.country_Name;
+      const cursor = touristsPlaceCollection.find({ country_Name: country_name });
+      const result = await cursor.toArray();
+      res.send(result);
     })
 
-    app.get('/myList/:email', async(req, res) => {
-        const email = req.params.email;
-        const cursor = touristsPlaceCollection.find({user_email : email});
-        const result = await cursor.toArray();
-        res.send(result);
+    app.get('/myList/:email', async (req, res) => {
+      const email = req.params.email;
+      const cursor = touristsPlaceCollection.find({ user_email: email });
+      const result = await cursor.toArray();
+      res.send(result);
     })
 
-    app.post('/addTouristsSpot', async(req, res)=>{
+    app.post('/addTouristsSpot', async (req, res) => {
       const newTouristsSpot = req.body;
       console.log(newTouristsSpot);
       const result = await touristsPlaceCollection.insertOne(newTouristsSpot);
       res.send(result);
     })
 
-    app.put('/touristsSpot/:id', async(req, res) => {
+    app.put('/touristsSpot/:id', async (req, res) => {
       const id = req.params.id;
-      const filter = {_id: new ObjectId(id)};
+      const filter = { _id: new ObjectId(id) };
       const options = { upsert: true };
       const updatedTouristsSpot = req.body;
 
       const updateTouristsSpot = {
         $set: {
-            tourists_spot_name: updatedTouristsSpot.tourists_spot_name,
-            country_Name: updatedTouristsSpot.country_Name,
-            total_visitors_per_year: updatedTouristsSpot.total_visitors_per_year,
-            location: updatedTouristsSpot.location,
-            short_description: updatedTouristsSpot.short_description,
-            average_cost: updatedTouristsSpot.average_cost,
-            seasonality: updatedTouristsSpot.seasonality,
-            travel_time: updatedTouristsSpot.travel_time,
-            image: updatedTouristsSpot.image,
+          tourists_spot_name: updatedTouristsSpot.tourists_spot_name,
+          country_Name: updatedTouristsSpot.country_Name,
+          total_visitors_per_year: updatedTouristsSpot.total_visitors_per_year,
+          location: updatedTouristsSpot.location,
+          short_description: updatedTouristsSpot.short_description,
+          average_cost: updatedTouristsSpot.average_cost,
+          seasonality: updatedTouristsSpot.seasonality,
+          travel_time: updatedTouristsSpot.travel_time,
+          image: updatedTouristsSpot.image,
         }
       }
 
@@ -98,18 +102,18 @@ async function run() {
     })
 
 
-    app.delete('/touristsSpot/:id', async(req, res) => {
-        const id = req.params.id;
-        const query = {_id: new ObjectId(id)};
-        const result = await touristsPlaceCollection.deleteOne(query);
-        res.send(result);
+    app.delete('/touristsSpot/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await touristsPlaceCollection.deleteOne(query);
+      res.send(result);
     })
 
 
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
@@ -119,12 +123,12 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get('/',(req, res) => {
-    res.send('dream destination server is running...');
+app.get('/', (req, res) => {
+  res.send('dream destination server is running...');
 })
 
 
 app.listen(port, () => {
-    console.log(`dream destination server is running in the ${port} port`);
+  console.log(`dream destination server is running in the ${port} port`);
 })
 
